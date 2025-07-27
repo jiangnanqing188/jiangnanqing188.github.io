0a6e0633f586f88661e06d3c0762224f39111327
jiangnanqing188 <jiangnanqingwqw@163.com>
2025-07-27 13:55:05 +0800

    Site updated: 2025-07-27 13:55:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/26/hello-world/","2025/07/26/test/"];function toRandomPost(){
+var posts=["2025/07/26/hello-world/","2025/07/26/test/","2025/07/27/py学习/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
