4c4c5bcb60607e78bd750c237734f5104d03d472
因你而在的梦 <jiangnanqingwqw@163.com>
2026-02-06 22:29:18 +0800

    Site Update: 2026/02/06 周五 22:29:18.10

--- public/anzhiyu/random.js
+++ public/anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/28/ISCC微型工厂模拟赛道经验分享/","2026/01/29/YOLOv5模型训练/","2025/07/27/py学习笔记/"];function toRandomPost(){
+var posts=["2026/01/29/YOLOv5模型训练/","2026/01/28/ISCC微型工厂模拟赛道经验分享/","2025/07/27/py学习笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
